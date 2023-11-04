dd6014738c1f1f892e7dd0309f728329b77f4ae9
kimyurie <9911dbfl@naver.com>
2023-11-04 14:02:13 +0900

    Add: 타입스크립트 기본 타입 정리

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var 이름 = 'kim';
 // 아래 변수엔 string 담긴 array만 들어올 수 있다
 var 어레이 = ['kim', 'park'];
 // object 타입 지정
-// 특정 속성이 선택사항이면 name? : string 이런식으로
-var 오브젝트 = { name: '' };
+var 오브젝트 = { name: 'kim' };
+// 특정 속성이 선택사항이면 아래처럼
+// name이라는 속성이 들어올 수도 안들어 올수도 있음을 나타낸다 (안들어와도 에러안남)
+var 오브젝트2 = {};
 // 다양한 타입이 들어올 수 있게 하려면 Union type
 var 다양하게 = '123';
 var 타입 = 123;
@@ -14,6 +16,10 @@ function 함수(x) {
     return x * 2;
 }
 var john = [123, true];
+var 재민 = {
+    name: 'kim',
+    age: 50
+};
 var 철수 = {
     age: 50,
     weight: 100,
---
+++ index2.ts
@@ -0,0 +1,39 @@
+// [ 타입스크립트 기본 타입 정리(primitive types) ]
+// 변수에 타입 지정 가능 = 변수에 실드 씌우는 것임
+let a :string = 'kim';
+// a = 123; // 타입이 실수로 변경될 때 에러내줌
+
+let age :number = 50;
+let answer :boolean = true; 
+// 위 외에도 undefined, null 타입으로도 지정 가능
+
+// 변수 하나에 여러자료 넣고 싶으면 array 자료형 쓰기
+// array자료에 아래와 같이 타입지정도 가능 (문자만 담긴 array 가능이라는 뜻)
+let member :string[] = ['kim', 'park']
+
+// array에 숫자랑 문자 동시에 넣고 싶으면? 나중에 배울 union타입 쓰면 된다
+
+// 변수 하나에 여러자료 넣고 싶으면 object 자료형 써도 가능
+// object 자료에도 아래와 같이 타입지정도 가능 
+// (이 변수엔 object만 들어올 수 있으며 member1, member2 옆엔 string이 와야 한다는 뜻)
+let member2 :{member1 : string, member2: string} = { member1 : 'kim', member2 : 'park'}
+
+
+// 근데 타입지정 모든 변수에 다 쓸 필요없고 원래 자동으로 되서 타입지정 문법 생략해도 된다
+let ccc = 'kim';
+
+// [과제]
+// Q1. 여러분의 이름, 나이, 출생지역을 변수로 각각 저장해봅시다. 
+let aa :string = '유리';
+let bb :number = 1;
+let area :string = '경기';
+
+// Q2. 여러분이 가장 좋아하는 곡과 가수이름을 변수에 object 자료형으로 담아보십시오.
+let music :{song : string, singer : string}= {song : 'circle', singer:'post-malone'}
+
+// Q3. 다음과 같이 생긴 자료의 타입지정을 해보도록 합시다.
+let project :{people :string[], days : number, started : boolean} = {
+    people : ['kim', 'park'],
+    days : 30,
+    started : true,
+}
